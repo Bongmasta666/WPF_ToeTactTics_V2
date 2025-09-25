c9fe8b5d3d340b0dffd1cd22ecec3c545040241f
Bongmasta666 <mmillar420@gmail.com>
2025-09-25 09:13:03 -0400

    Add project files.

---
+++ CustomPopUp.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ToeTactTics_V2
+{
+    public partial class CustomPopUp : Window
+    {
+        public CustomPopUp()
+        {
+            InitializeComponent();
+            textBoxPlayerX.Focus();
+        }
+
+        public void OnSubmit(Object sender, RoutedEventArgs e)
+        {
+            textBoxPlayerX.Text.Trim();
+            textBoxPlayerO.Text.Trim();
+            if (textBoxPlayerO.Text.Length > 3 && textBoxPlayerO.Text.Length > 3)
+            {
+                DialogResult = true;
+                Close();
+            }
+        }
+
+        public void OnCancel(Object sender, RoutedEventArgs e) 
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+/*
+  Author: Michael Millar
+  Date: 09-25-2025
+  Description:
+    A quick WPF Tic Tac Toe type game. 
+    Keeps track of 2 users and their score, evaultes board state on each play, and alternates between players.
+*/
+namespace ToeTactTics_V2
+{
+    public partial class MainWindow : Window
+    {
+        string playerXName = "";
+        string playerOName = "";
+        int playerXWins = 0;
+        int playerOWins = 0;
+        bool isPlayerXTurn = true;
+
+        Button[] buttons = [];
+        int buttonsRemaining = 9;
+        public MainWindow()
+        {
+            InitializeComponent();
+            buttons = [button1, button2, button3, button4, button5, 
+                         button6, button7, button8, button9];
+        }
+
+        public void OnNewGame(Object sender , RoutedEventArgs e) => NewGame();
+        public void NewGame()
+        {
+            ShowUsernameDialog();
+        }
+
+        public void StartGame()
+        {
+            SetRandomPlayer();
+            ShowActivePlayer();
+            SetBoardState(true);
+        }
+
+        public void OnSquareSelected(Object sender, RoutedEventArgs e)
+        {
+            Button target = (Button)sender;
+            target.Content = (isPlayerXTurn) ? "X" : "O";
+            target.IsEnabled = false;
+
+            isPlayerXTurn = !isPlayerXTurn;
+            ShowActivePlayer();
+        }
+
+        public bool CheckLine(Button first, Button second, Button third)
+        {
+            return first.Content == second.Content && second.Content == third.Content && first.Content.ToString() != "";
+        }
+
+        public void SetBoardState(bool isActive)
+        {
+            foreach (Button button in buttons) { button.IsEnabled = isActive; }
+        }
+
+        public void ShowUsernameDialog()
+        {
+            CustomPopUp usernamePop = new CustomPopUp();
+            usernamePop.Title = "Username Selection";
+            usernamePop.Owner = this;
+            usernamePop.ShowDialog();
+
+            if (usernamePop.DialogResult == true)
+            {
+                playerXName = usernamePop.textBoxPlayerX.Text;
+                playerOName = usernamePop.textBoxPlayerO.Text;
+                UpdateUserInfo();
+                StartGame();
+            }
+        }
+
+        public void SetRandomPlayer()
+        {
+            Random random = new Random();
+            int number = random.Next(1, 101);
+            isPlayerXTurn = number % 2 == 0; 
+        }
+
+        public void ShowActivePlayer()
+        {
+            labelPlayerTurn.Content = (isPlayerXTurn) ? $"{playerXName}'s Turn" : $"{playerOName}'s Turn";
+        }
+
+        public void UpdateUserInfo()
+        {
+            labelPlayerXInfo.Content = $"Player (X)  {playerXName}  : Wins - {playerXWins}";
+            labelPlayerOInfo.Content = $"Player (O)  {playerOName}  : Wins - {playerOWins}";
+        }
+
+        public void OnQuitGame(Object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
